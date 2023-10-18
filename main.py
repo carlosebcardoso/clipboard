60897c6ded6acf022e1396eadd52f54eb9182e6c
carlos.eduardo <carlosebcardoso@gmail.com>
2023-10-18 16:47:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import pygame
-# import keyboard
 import pyperclip
 import threading
+# import time
+# import keyboard
 
+# pygame and screen init
 pygame.init()
-
 pygame.display.set_caption('Clipboard')
 screen = pygame.display.set_mode((350, 585))
-icon = pygame.image.load('./clipboard/icon.jpg')
-pygame.display.set_icon(icon)
 
-dark_mode_icon = pygame.image.load('./clipboard/dark_mode.svg')
-delete_icon = pygame.image.load('./clipboard/delete.svg')
+# global variables
+dark_mode_icon = pygame.image.load('dark_mode.svg')
+delete_icon = pygame.image.load('delete.svg')
 
 dark_mode_rect = pygame.Rect(5, 5, 24, 24)
 delete_rect = pygame.Rect(321, 5, 24, 24)
-
-text_font = pygame.font.SysFont("Helvetica", 16)
-
-clipboard = []
 
 dark_mode = True
 txt_color = 'white'
@@ -40,82 +36,93 @@ def switchDarkMode():
         bg_color = 'black'
         dark_mode = True
 
-def deleteAll():
-    for clip in clipboard:
-        clipboard.pop()
+class Clipboard:
+    def __init__(self):
+        self.board = []
 
-def organizeClips():
-    global clipboard
+    def addToClipboard(self):
+        while True:
+            clipText = pyperclip.waitForNewPaste()
 
-    for clip in clipboard:
-        id = clipboard.index(clip)
-        clip['rect'].y = (id*50) + (id*5) + 35
+            # removes empty
+            if self.board[0] == '':
+                self.board.pop(0)
 
-def createClip(text):
-    global clipboard
-    rect = pygame.Rect((5, 25, 340, 50))
+            # removes duplicates
+            for clip in self.board:
+                if clipText == clip.text:
+                    self.board.remove(clip)
 
-    newClip = {
-        'text': text,
-        'rect': rect,
-    }
-    clipboard.insert(0, newClip)
-    organizeClips()
+            # limits clipboard to 10 clips
+            if len(self.board) >= 10:
+                self.board.pop(-1)
 
-firstClip = createClip(pyperclip.paste())
+            # creates a new clip with clipText
+            newClip = Clip(clipText)
+            newClip.insertClip(self)
+    
+    def organizeClips(self):
+        for clip in self.board:
+            id = self.board.index(clip)
+            clip.rect.y = (id*50) + (id*5) + 35
+    
+    def deleteAll(self):
+        for i in range(len(self.board)):
+            self.board.pop()
 
-# def openClipBoard():
-#     print('open')
+        firstClip = Clip(pyperclip.paste())
+        firstClip.insertClip(self)
 
-# keyboard.add_hotkey('shift+f2', openClipBoard, args=[])
+clipboard = Clipboard()
+
+class Clip:
+    def __init__(self, text):
+        self.text = text
+        self.rect = pygame.Rect((5, 25, 340, 50))
+
+    def insertClip(self, clipboard):
+        clipboard.board.insert(0, self)
+        clipboard.organizeClips()
+
+firstClip = Clip(pyperclip.paste())
+firstClip.insertClip(clipboard)
 
 def drawText(text, x, y):
-    global text_font
-    txt = text_font.render(text, True, txt_color)
+    txt = pygame.font.SysFont("Helvetica", 16).render(text, True, txt_color)
     screen.blit(txt, (x, y), (0, 0, 330, 50))
     screen.blit(txt, (x, y), (330, -22, 330, 50))
 
-def addToClipBoard():
-    while True:
-        clipText = pyperclip.waitForNewPaste()
-
-        for i in clipboard:
-            if clipText == i['text']:
-                clipboard.remove(i)
-
-        if len(clipboard) >= 10:
-            clipboard.pop(9)
-
-        createClip(clipText)
-
-threading.Thread(target=addToClipBoard, daemon=True).start()
+threading.Thread(target=clipboard.addToClipboard, daemon=True).start()
 
 run = True
 while run:
     screen.fill(bg_color)
 
-    for clip in clipboard:
-        pygame.draw.rect(screen, (txt_color), clip['rect'], 2, 2)
-        drawText(clip['text'], clip['rect'].x + 5, clip['rect'].y + 5)
+    for clip in clipboard.board:
+        pygame.draw.rect(screen, (txt_color), clip.rect, 2, 2)
+        drawText(clip.text, clip.rect.x + 5, clip.rect.y + 5)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
         elif event.type == pygame.MOUSEBUTTONDOWN and dark_mode_rect.collidepoint(event.pos):
             switchDarkMode()
-        elif event.type == pygame.MOUSEBUTTONDOWN and delete_rect.collidepoint(event.pos):
-            deleteAll()
-        elif event.type == pygame.MOUSEBUTTONUP:
-            for clip in clipboard:
-                if clip['rect'].collidepoint(event.pos):
-                    pyperclip.copy(clip['text'])
 
-    # delete icon
-    pygame.draw.circle(screen, ('white'), (333, 17), 12)
-    screen.blit(delete_icon, (delete_rect.x, delete_rect.y))
+        elif event.type == pygame.MOUSEBUTTONDOWN and delete_rect.collidepoint(event.pos):
+            clipboard.deleteAll()
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            for clip in clipboard.board:
+                if clip.rect.collidepoint(event.pos):
+                    pyperclip.copy(clip.text)
+
     # dark mode icon
     pygame.draw.circle(screen, ('white'), (17, 17), 12)
     screen.blit(dark_mode_icon, (5, 5))
+    # delete icon
+    pygame.draw.circle(screen, ('white'), (333, 17), 12)
+    screen.blit(delete_icon, (delete_rect.x, delete_rect.y))
 
     pygame.display.flip()
 pygame.quit()
